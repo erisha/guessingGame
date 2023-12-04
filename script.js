9740797a4bea6971f32d44482886c6c7d3347d94
Erisha Roach <forever.erisha@gmail.com>
2023-12-04 09:46:42 -0500

    fixed bug

--- script.js
+++ script.js
@@ -8,36 +8,36 @@
 //picks mysteryNum between 1-10
 const mysteryNum = Math.floor(Math.random() * 10) + 1;
 
-
+//max attempts
+let maxAttempts = 3;
 
 // play the guessingGame
 function guessingGame() {
-    //max attempts
-    let maxAttempts = 3;
-  for ( let attempts = 1; attempts <= maxAttempts; attempts++){
+  for (let attempts = 1; attempts <= maxAttempts; attempts++){
     // prompts user to guess a number
-    const userInput = window.parseInt(prompt(`Attempt ${attempts} : Guess a number 1-10`));
-    window.alert(`You guessed ${userInput}`);
+    const userInput = parseInt(prompt(`Attempt ${attempts}/ ${maxAttempts} : Guess a number 1-10`));
+        // alert(`You guessed ${userInput}`);
+
     // check if userInputinput is a valid number 
     if (isNan(userInput)){
-      window.alert(`Error: Please enter a valid number`);
+      alert(`Error: Please enter a valid number`);
       attempts --; //substacts after each incorrect attempt
       continue; 
     }  
     // check if userInput is correct
       if (mysteryNum === userInput) {
-        window.alert(`Winner! ${mysteryNum} is correct you guessed it in ${attempts}`);
+        alert(`Winner! ${mysteryNum} is correct you guessed it in ${attempts}`);
       } 
       // wrong answer - give hint
       else if (mysteryNum > userInput) {
-        window.alert(`Nope. Guess higher....try again`);
+        alert(`Nope. Guess higher....try again`);
       } else if (mysteryNum < userInput) {
-        window.alert(`Nope. Guess a lower number....try again`);
+        alert(`Nope. Guess a lower number....try again`);
       }
     }
     // out of guesses/ attempts
     if (attempts > maxAttemps){
-      window.alert(`Game Over! You're out of guesses.`)
+      alert(`Game Over! You're out of guesses.`)
     }
   }
 
